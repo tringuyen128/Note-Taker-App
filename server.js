41d759ec9afa3e74ed66e70a9ee381f537cb0e2d
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-03-26 09:48:10 -0700

    update routes and public file

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 8080;
-// configure express use
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
 app.use(express.json());
 
-
-
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Now listening on PORT: ${PORT}`);
-});
+    console.log(`Server is now on the ${PORT}`);
+  });
